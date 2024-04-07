4e101926036282c99c2356268d2e686975e40042
tanushaayer <tanushastan@gmail.com>
2024-04-07 02:23:52 -0500

    AOP exception and its handler

---
+++ src/main/java/edu/miu/springaop/aop/exception/ErrorDetails.java
@@ -0,0 +1,18 @@
+package edu.miu.springaop.aop.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetails {
+    private LocalDateTime timestamp;
+    private String message;
+    private String path;
+}
