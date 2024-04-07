e9157b0b552ed8341b60b1c1939d3b5abbe62291
tanushaayer <tanushastan@gmail.com>
2024-04-07 02:24:12 -0500

    Adds dummy data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO user(name,age,address) VALUES("Hari", 23, "Fairfield"),
+                                         ("Shyam", 53, "Fairfield");
+INSERT INTO comment(user_id,comment) VALUES (1, "Good"),
+                                            (1, "Bad"),
+                                            (2, "Good");
